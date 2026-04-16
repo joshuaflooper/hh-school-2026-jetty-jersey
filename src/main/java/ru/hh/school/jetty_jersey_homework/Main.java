9f758577d5bac76568f23f77c6a08b1ccebfbdcb
Georgiy Podobin <gpodobin@yandex.ru>
2026-04-16 21:52:28 +0300

    Add Main class

---
+++ src/main/java/ru/hh/school/jetty_jersey_homework/Main.java
@@ -0,0 +1,32 @@
+package ru.hh.school.jetty_jersey_homework;
+
+import org.eclipse.jetty.ee11.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee11.servlet.ServletHolder;
+import org.eclipse.jetty.server.Server;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+public class Main {
+
+  static void main() throws Exception {
+    Server server = new Server(8080);
+    ServletContextHandler context = new ServletContextHandler("/");
+    server.setHandler(context);
+
+    ResourceConfig config = new ResourceConfig();
+//    config.register(MainResource.class);
+//    config.register(MessagesResource.class);
+
+    ServletHolder servletHolder = new ServletHolder(new ServletContainer(config));
+
+    context.addServlet(servletHolder, "/*");
+
+    try {
+      server.start();
+      server.join();
+    } finally {
+      server.destroy();
+    }
+  }
+
+}
