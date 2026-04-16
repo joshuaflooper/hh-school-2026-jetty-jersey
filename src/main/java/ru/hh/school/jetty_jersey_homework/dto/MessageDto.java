aa926214db45cefa846b61dced11ecdb9f72ef83
Georgiy Podobin <gpodobin@yandex.ru>
2026-04-17 01:45:57 +0300

    Update message dto

--- src/main/java/ru/hh/school/jetty_jersey_homework/dto/MessageDto.java
+++ src/main/java/ru/hh/school/jetty_jersey_homework/dto/MessageDto.java
@@ -2,5 +2,5 @@ package ru.hh.school.jetty_jersey_homework.dto;
 
 import java.time.LocalDateTime;
 
-public record MessageDto(int id, String senderIp, String text, LocalDateTime time) {
+public record MessageDto(Integer id, String senderIp, String text, LocalDateTime time) {
 }
